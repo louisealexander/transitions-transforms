a3d0b9636d9dd6ccbec3e0e27b764ab9d7238b7c
louisealexander <115469352+louisealexander@users.noreply.github.com>
2022-10-20 16:05:43 +0100

    20th October ease/hover polaroid

---
+++ javaScript20_10/myJavascriptFile.js
@@ -0,0 +1,17 @@
+// This is an empty Javascript File
+let tvCharacters = ["rick", "morty", "summer", "beth", "jerry"];
+console.log(tvCharacters);
+
+let text = "";
+for (let i = 0; i < tvCharacters.length; i++) {
+    let element = tvCharacters[i]
+    console.log(element);
+}
+
+    for (let i = 1; i < 12; i++){
+        console.log(i*i);
+    }
+
+let multiArray = {
+                    ["Seamus Aran", 74]
+                    ["Craig Beavid", 12]
